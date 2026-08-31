2eea40cb403a31e07ccb7814aa34263514fa7d8c
GrafTrahula666 <origtopg777@gmail.com>
2026-08-31 07:33:52 +0300

    Load Admin commercial UI polish styles

--- admin-platform/src/main.tsx
+++ admin-platform/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './responsive-fixes.css'
+import './ui-polish.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
